28b447f4746957c0f2c54a04bde18120e67c27ae
xuyifan <18301053@bjtu.edu.cn>
2021-04-17 09:45:46 +0800

    'init'

---
+++ communitybackend/src/main/java/com/xuyifan/communityback/CommunitybackApplication.java
@@ -0,0 +1,13 @@
+package com.xuyifan.communityback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitybackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitybackApplication.class, args);
+    }
+
+}
---
+++ communitybackend/src/test/java/com/xuyifan/communityback/CommunitybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyifan.communityback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitybackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
